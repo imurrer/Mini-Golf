05c5425ab232448ff7775f8f19336b2fe4e5b2f5
imurrer <33602604+imurrer@users.noreply.github.com>
2018-12-07 10:53:12 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main() {
        if (c == 'q') {
          break;
        }
-     }
+    
      
     //switch(c) {
      //   case 
@@ -54,7 +54,7 @@ int main() {
 
     gfx_color(0,43,4); // dropoff line
     gfx_line(200, 250, 400 , 250);
-  
+     }
   }
 }
     
