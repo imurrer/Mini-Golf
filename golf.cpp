a98bfd33a2c23e79ec0bb2692dfcb567bfab20ca
Hannah Sarkey <38545089+hsarkey@users.noreply.github.com>
2018-12-07 10:47:01 -0500

    Update golf.cpp

--- golf.cpp
+++ golf.cpp
@@ -1,1 +1,24 @@
 //golf.cpp
+//Bella Murrer and Hannah Sarkey
+
+
+void Golf::display() {
+  
+}
+
+void Golf::rotateMill() {
+  
+}
+
+bool Golf::throughMill() {
+  
+}
+
+float Golf::ballAngle() {
+
+}
+
+float Golf::ballSpeed() {
+
+}
+
