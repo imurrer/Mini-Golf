5529af83b3f2be1d71dfffb9db22e3409c2f47ac
imurrer <33602604+imurrer@users.noreply.github.com>
2018-12-07 10:59:12 -0500

    Update golf.cpp

--- golf.cpp
+++ golf.cpp
@@ -6,7 +6,13 @@
 using namespace std;
 
 Golf::Golf() {
-  
+   float ballx = 300, bally = 740, ballrad = 7.5;
+   float bx = 100, by = 550;
+   float mx = 200, my = 250;
+   float tx = 100, ty = 50;
+   float l = 400, w = 200;
+   float ml = 300, mw = 200;
+   float hx= 287.5 , hy= 537.5 , hl = 25, hw = 20, a1 =0, a2 =180;
 }
 
 Golf::~Golf() {
