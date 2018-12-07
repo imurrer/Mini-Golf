e6748d7c11add576994648e99bcb9e57515d0ca9
imurrer <33602604+imurrer@users.noreply.github.com>
2018-12-07 11:05:14 -0500

    Update golf.h

--- golf.h
+++ golf.h
@@ -4,13 +4,16 @@ class Golf {
   public:
     Golf();
     ~Golf();
-  rotateMill();
-  throughMill();
-  ballAngle();
-  ballSpeed();
+    void display();
+  /*
+    void rotateMill();
+    void throughMill();
+    void ballAngle();
+    void ballSpeed();*/
   private:
     float angWM;
     float bx, by, ty, tx, l, w;
     float mx, my, ml, mw;
     float hx, hy, hw, hl, a1, a2;
+    float arrowX, arrowY;
 }
