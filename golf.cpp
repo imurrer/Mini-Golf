28c4e8558bc48abf5c9500d4db7d7b7acfb5c139
imurrer <33602604+imurrer@users.noreply.github.com>
2018-12-08 15:22:38 -0500

    Update golf.cpp

--- golf.cpp
+++ golf.cpp
@@ -163,7 +163,7 @@ bool Golf::releaseball() {
       display();
       rotateMill();
       cout << radline << "radline" <<endl;
-      /*
+      
       if(radline <= 447.3 && radline > 357){
          cout << "1" << endl;
          dx = dx-minus; //do angle stuff with direction of ball
@@ -190,7 +190,8 @@ bool Golf::releaseball() {
          dy = dy-minus;
          minus +=3;
          cout << "5" << minus << endl;
-      }*/
+      }
+      
       cout << "ball points:" << endl;
       cout << ballx << " " << bally << " dy " << dy << endl;
       ballx = dx + ballx;
